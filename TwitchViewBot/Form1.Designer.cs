b65e8fe76764e3e821c50be417ea7a28b7016fa3
Luck <Luck@DEATH-TO-JEWS>
2025-03-16 06:17:05 +0900

    Add project files.

---
+++ TwitchViewBot/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace TwitchViewBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameBox = new System.Windows.Forms.TextBox();
+            this.userNameLbl = new System.Windows.Forms.Label();
+            this.numViewersTxt = new System.Windows.Forms.TextBox();
+            this.NumOfViewersLbl = new System.Windows.Forms.Label();
+            this.LogTxtBx = new System.Windows.Forms.TextBox();
+            this.txtLogLbl = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.liveViewerLabel = new System.Windows.Forms.Label();
+            this.refreshInterval = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.refreshInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userNameBox
+            // 
+            this.userNameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameBox.Location = new System.Drawing.Point(17, 51);
+            this.userNameBox.Name = "userNameBox";
+            this.userNameBox.Size = new System.Drawing.Size(400, 38);
+            this.userNameBox.TabIndex = 0;
+            // 
+            // userNameLbl
+            // 
+            this.userNameLbl.AutoSize = true;
+            this.userNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLbl.Location = new System.Drawing.Point(12, 23);
+            this.userNameLbl.Name = "userNameLbl";
+            this.userNameLbl.Size = new System.Drawing.Size(189, 25);
+            this.userNameLbl.TabIndex = 1;
+            this.userNameLbl.Text = "Twitch UserName:";
+            // 
+            // numViewersTxt
+            // 
+            this.numViewersTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numViewersTxt.Location = new System.Drawing.Point(17, 133);
+            this.numViewersTxt.Name = "numViewersTxt";
+            this.numViewersTxt.Size = new System.Drawing.Size(400, 38);
+            this.numViewersTxt.TabIndex = 2;
+            // 
+            // NumOfViewersLbl
+            // 
+            this.NumOfViewersLbl.AutoSize = true;
+            this.NumOfViewersLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumOfViewersLbl.Location = new System.Drawing.Point(12, 105);
+            this.NumOfViewersLbl.Name = "NumOfViewersLbl";
+            this.NumOfViewersLbl.Size = new System.Drawing.Size(201, 25);
+            this.NumOfViewersLbl.TabIndex = 3;
+            this.NumOfViewersLbl.Text = "Number of Viewers:";
+            // 
+            // LogTxtBx
+            // 
+            this.LogTxtBx.Location = new System.Drawing.Point(15, 448);
+            this.LogTxtBx.Multiline = true;
+            this.LogTxtBx.Name = "LogTxtBx";
+            this.LogTxtBx.Size = new System.Drawing.Size(402, 454);
+            this.LogTxtBx.TabIndex = 4;
+            // 
+            // txtLogLbl
+            // 
+            this.txtLogLbl.AutoSize = true;
+            this.txtLogLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogLbl.Location = new System.Drawing.Point(12, 420);
+            this.txtLogLbl.Name = "txtLogLbl";
+            this.txtLogLbl.Size = new System.Drawing.Size(55, 25);
+            this.txtLogLbl.TabIndex = 5;
+            this.txtLogLbl.Text = "Log:";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(17, 326);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(165, 73);
+            this.startBtn.TabIndex = 6;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(252, 326);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(165, 73);
+            this.stopBtn.TabIndex = 7;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Bots Watching: ";
+            // 
+            // liveViewerLabel
+            // 
+            this.liveViewerLabel.AutoSize = true;
+            this.liveViewerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.liveViewerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.liveViewerLabel.Location = new System.Drawing.Point(288, 267);
+            this.liveViewerLabel.Name = "liveViewerLabel";
+            this.liveViewerLabel.Size = new System.Drawing.Size(24, 25);
+            this.liveViewerLabel.TabIndex = 9;
+            this.liveViewerLabel.Text = "0";
+            this.liveViewerLabel.Click += new System.EventHandler(this.liveViewerLabel_Click);
+            // 
+            // refreshInterval
+            // 
+            this.refreshInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshInterval.Location = new System.Drawing.Point(293, 199);
+            this.refreshInterval.Name = "refreshInterval";
+            this.refreshInterval.Size = new System.Drawing.Size(124, 30);
+            this.refreshInterval.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Refresh Interval in Minutes:";
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(420, -3);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(18, 935);
+            this.vScrollBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 932);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.refreshInterval);
+            this.Controls.Add(this.liveViewerLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.txtLogLbl);
+            this.Controls.Add(this.LogTxtBx);
+            this.Controls.Add(this.NumOfViewersLbl);
+            this.Controls.Add(this.numViewersTxt);
+            this.Controls.Add(this.userNameLbl);
+            this.Controls.Add(this.userNameBox);
+            this.Name = "Form1";
+            this.Text = "Twitch View Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.refreshInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userNameBox;
+        private System.Windows.Forms.Label userNameLbl;
+        private System.Windows.Forms.TextBox numViewersTxt;
+        private System.Windows.Forms.Label NumOfViewersLbl;
+        private System.Windows.Forms.TextBox LogTxtBx;
+        private System.Windows.Forms.Label txtLogLbl;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label liveViewerLabel;
+        private System.Windows.Forms.NumericUpDown refreshInterval;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ TwitchViewBot/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TwitchViewBot
+{
+    public partial class Form1: Form
+    {
+        private static Process torProcess;
+        private static List<ChromeDriver> drivers = new List<ChromeDriver>();
+        private static int viewerCount = 2; // Default viewer count
+        private static string channelName = "LassieLou"; // Default channel
+        private Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        private bool keepRefreshing = false;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+            LoadSettings();
+            Text = "Twitch Viewer Bot v1.0";
+
+        }
+
+        private void LoadSettings()
+        {
+            userNameBox.Text = config.AppSettings.Settings["ChannelName"]?.Value ?? "LassieLou";
+            numViewersTxt.Text = config.AppSettings.Settings["ViewerCount"]?.Value ?? "2";
+            refreshInterval.Value = int.TryParse(config.AppSettings.Settings["RefreshInterval"]?.Value, out int interval) ? interval : 10;
+        }
+
+        private void InitializeCustomComponents()
+        {
+            startBtn.Click += StartBtn_Click;
+            stopBtn.Click += StopBtn_Click;
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void SaveSettings()
+        {
+            config.AppSettings.Settings.Remove("ChannelName");
+            config.AppSettings.Settings.Add("ChannelName", userNameBox.Text);
+            config.AppSettings.Settings.Remove("ViewerCount");
+            config.AppSettings.Settings.Add("ViewerCount", numViewersTxt.Text);
+            config.AppSettings.Settings.Remove("RefreshInterval");
+            config.AppSettings.Settings.Add("RefreshInterval", refreshInterval.Value.ToString());
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private async void StartBtn_Click(object sender, EventArgs e)
+        {
+            startBtn.Enabled = false;
+            if (!int.TryParse(numViewersTxt.Text, out viewerCount) || viewerCount < 1)
+            {
+                Log("Error: Please enter a valid number of viewers (1 or more).");
+                startBtn.Enabled = true;
+                return;
+            }
+            channelName = userNameBox.Text.Trim();
+            if (string.IsNullOrEmpty(channelName))
+            {
+                Log("Error: Please enter a channel name.");
+                startBtn.Enabled = true;
+                return;
+            }
+
+            SaveSettings();
+            Log("Starting Twitch Viewer Bot...");
+            try
+            {
+                keepRefreshing = true;
+                await LaunchSequentialViewers();
+                await Task.Run(() => RefreshViewers()); // Start refresh loop
+                Log("All viewers launched.");
+            }
+            catch (Exception ex)
+            {
+                Log($"Error: {ex.Message}");
+            }
+            startBtn.Enabled = true;
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            Log("Stopping viewers...");
+            keepRefreshing = false;
+            Cleanup();
+            Log("All processes terminated.");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            keepRefreshing = false;
+            Cleanup();
+            Application.Exit();
+        }
+
+        private void StartTor()
+        {
+            string torPath = @"C:\Users\Luck\Desktop\Tor Browser\Browser\TorBrowser\Tor\tor.exe"; // Adjust path to your path
+            torProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = torPath,
+                    Arguments = "--SocksPort 9050",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            torProcess.Start();
+            Log("Starting Tor...");
+            Task.Delay(10000).Wait(); // Wait for Tor to bootstrap
+            Log("Tor initialized.");
+        }
+
+        private async Task LaunchSequentialViewers()
+        {
+            for (int i = 0; i < viewerCount; i++)
+            {
+                if (torProcess != null && !torProcess.HasExited)
+                {
+                    torProcess.Kill();
+                    Log("Cycling Tor circuit...");
+                }
+                StartTor();
+                await Task.Run(() => LaunchViewer(i));
+                UpdateViewerCount(); // Update after each viewer
+            }
+        }
+
+        private void LaunchViewer(int viewerId)
+        {
+            try
+            {
+                var options = new ChromeOptions();
+                options.AddArgument("--headless"); // Uncomment after testing
+                options.AddArgument("--proxy-server=socks5://127.0.0.1:9050");
+                options.AddArgument("--no-sandbox");
+                options.AddArgument("--autoplay-policy=no-user-gesture-required");
+                options.AddArgument("--enable-unsafe-swiftshader");
+                options.AddArgument("--disable-gpu-sandbox");
+                options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
+
+                var service = ChromeDriverService.CreateDefaultService();
+                service.SuppressInitialDiagnosticInformation = true;
+                service.HideCommandPromptWindow = true;
+
+                var driver = new ChromeDriver(service, options);
+                lock (drivers) { drivers.Add(driver); }
+
+                Log($"Viewer {viewerId + 1}: Connecting to {channelName}");
+                driver.Navigate().GoToUrl($"https://www.twitch.tv/{channelName}");
+                Task.Delay(30000).Wait(); // Wait for page load
+
+                var videoExists = driver.ExecuteScript("return !!document.querySelector('video');");
+                Log($"Viewer {viewerId + 1}: Video element exists: {videoExists}");
+                if (Convert.ToBoolean(videoExists))
+                {
+                    driver.ExecuteScript("document.querySelector('video')?.play();");
+                    Log($"Viewer {viewerId + 1}: Video play attempted.");
+
+                    driver.ExecuteScript("document.querySelector('video')?.play();");
+                    Log($"Viewer {viewerId + 1}: Video play attempted.");
+
+                    // Set lowest quality (160p)
+                    driver.ExecuteScript(@"
+                var player = document.querySelector('.video-player');
+                if (player) {
+                    var settingsButton = document.querySelector('button[data-a-target=""player-settings-button""]');
+                    if (settingsButton) {
+                        settingsButton.click(); // Open settings
+                        setTimeout(function() {
+                            var qualityMenu = document.querySelector('button[data-a-target=""player-settings-menu-item-quality""]');
+                            if (qualityMenu) {
+                                qualityMenu.click(); // Open quality options
+                                setTimeout(function() {
+                                    var qualityOptions = document.querySelectorAll('input[data-a-target=""tw-radio""]');
+                                    if (qualityOptions.length > 0) {
+                                        var lowestQuality = qualityOptions[qualityOptions.length - 1]; // Last option is lowest (160p)
+                                        lowestQuality.click();
+                                    }
+                                }, 1000);
+                            }
+                        }, 1000);
+                    }
+                }
+            ");
+                    Task.Delay(100).Wait(); // Wait for quality change to apply
+                    Log($"Viewer {viewerId + 1}: Set to lowest quality (160p).");
+                
+
+            }
+                else
+                {
+                    var errorText = driver.FindElement(By.XPath("//*[@id=\'root\']/div/div[1]/div/main/div[1]/div[3]/div/div/div[2]/div/div[2]/div/div[3]/div/div/div[7]/div/div[3]/button")).Enabled;
+                    if (errorText)
+                    {
+                        Log($"Viewer {viewerId + 1}: Error #2000 detected. Refreshing...");
+                        driver.Navigate().Refresh();
+                        Task.Delay(15000).Wait();
+                    }
+                    else
+                    {
+                        Log($"Viewer {viewerId + 1}: No video found, no Error #2000 detected.");
+                    }
+                }
+
+                Log($"Viewer {viewerId + 1}: Page loaded. Title: {driver.Title}");
+            }
+            catch (Exception ex)
+            {
+                Log($"Viewer {viewerId + 1} failed: {ex.Message}");
+                lock (drivers) { drivers.RemoveAll(d => d == null || d.SessionId == null); } // Clean up failed drivers
+                UpdateViewerCount();
+            }
+        }
+
+        private async Task RefreshViewers()
+        {
+            while (keepRefreshing)
+            {
+                int minutes = (int)refreshInterval.Value;
+                if (minutes > 0)
+                {
+                    await Task.Delay(minutes * 60 * 1000); // Convert to milliseconds
+                    lock (drivers)
+                    {
+                        foreach (var driver in drivers)
+                        {
+                            try
+                            {
+                                driver.Navigate().Refresh();
+                                Log($"Refreshed viewer: {driver.Url}");
+                            }
+                            catch (Exception ex)
+                            {
+                                Log($"Refresh failed: {ex.Message}");
+                                drivers.Remove(driver); // Remove dead driver
+                                UpdateViewerCount();
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    await Task.Delay(1000); // Avoid tight loop if 0
+                }
+            }
+        }
+
+        private void UpdateViewerCount()
+        {
+            //this is completely wrong wtf
+            int count = drivers.Count;
+            if (liveViewerLabel.InvokeRequired)
+                liveViewerLabel.Invoke(new Action(() => liveViewerLabel.Text = $"Live Viewers: {count}"));
+            else
+                liveViewerLabel.Text = $"Live Viewers: {count}";
+        }
+
+        private void KillAllProcesses()
+        {
+            string[] processNames = { }; // Empty array?
+            foreach (var processName in processNames) { /* ... */ }
+
+            var allProcesses = Process.GetProcesses();
+            foreach (var process in allProcesses)
+            {
+                try
+                {
+                    if (!process.HasExited && !string.IsNullOrEmpty(process.MainWindowTitle) &&
+                        process.MainWindowTitle.Contains(userNameBox.Text)) ;
+                    {
+                        process.Kill();
+                        process.WaitForExit(1000);
+                        Log($"Terminated process with 'chromedriver.exe' in title (PID: {process.Id}, Name: {process.ProcessName})");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log($"Failed to kill process with 'chromedriver.exe' in title (PID: {process.Id}): {ex.Message}");
+                }
+                finally
+                {
+                    process.Dispose();
+                }
+            }
+        }
+
+
+
+        private void Cleanup()
+        {
+            lock (drivers) // Add thread safety
+            {
+                foreach (var driver in drivers.ToList()) // Use ToList() to avoid collection modification issues
+                {
+                    try
+                    {
+                        if (driver != null)
+                        {
+                            driver.Close();  // Close the browser window
+                            driver.Quit();   // Terminate the driver process
+                            Log($"Closed ChromeDriver instance.");
+                            GC.Collect();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log($"Error closing driver: {ex.Message}");
+                    }
+                }
+                drivers.Clear(); // Clear the list after cleanup
+            }
+
+            if (torProcess != null && !torProcess.HasExited)
+            {
+                try
+                {
+                    torProcess.Kill();
+                    torProcess.WaitForExit(2000); // Wait up to 2 seconds for clean exit
+                    Log("Tor process terminated.");
+                }
+                catch (Exception ex)
+                {
+                    Log($"Error stopping Tor: {ex.Message}");
+                }
+                finally
+                {
+                    torProcess = null;
+                }
+            }
+
+            UpdateViewerCount();
+        }
+
+
+
+
+        private void Log(string message)
+        {
+            if (LogTxtBx.InvokeRequired)
+            {
+                LogTxtBx.Invoke(new Action(() =>
+                {
+                    LogTxtBx.AppendText($"{DateTime.Now:HH:mm:ss} | {message}{Environment.NewLine}");
+                    LogTxtBx.ScrollToCaret();
+                }));
+            }
+            else
+            {
+                LogTxtBx.AppendText($"{DateTime.Now:HH:mm:ss} | {message}{Environment.NewLine}");
+                LogTxtBx.ScrollToCaret();
+            }
+        }
+
+        private void liveViewerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
